b36bb2a44df5a3afc5181f9a2c79d01c791790f9
Marcus Tsai <maxplayerr@gmail.com>
2021-12-01 12:13:46 +0800

    Update createuser.sh
    add password

--- createuser.sh
+++ createuser.sh
@@ -5,8 +5,9 @@ username=$1
 
 # Create User Account by RedHat(CentOS)
 /usr/sbin/useradd $username
+echo {{Password}} |passwd --stdin
 mkdir -p /home/$username/.ssh
 chmod 700 /home/$username/.ssh && chown $username:$username /home/$username/.ssh
-/usr/bin/cp ./authorized_keys /home/$username/.ssh/authorized_keys
-/usr/bin/chmod 700 /home/$username/.ssh/authorized_keys
+cp ./authorized_keys /home/$username/.ssh/authorized_keys
+chmod 700 /home/$username/.ssh/authorized_keys
 chown $username:$username /home/$username/.ssh/authorized_keys
